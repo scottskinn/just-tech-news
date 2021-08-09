04fdd1285322449c1b35beae7a6bb4a6aedae161
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-08-09 11:30:51 -0400

    user modal

--- models/User.js
+++ models/User.js
@@ -8,6 +8,38 @@ class User extends Model {}
 User.init(
   {
     // TABLE COLUMN DEFINITIONS GO HERE
+    // define an id column
+    id: {
+        // use the special sequelize datatypes objest provide what type of data it is
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    // define a username column
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    // define an email column
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // there cannot be any duplicate email values in this table
+        unique: true,
+        // if allowNull is set to false, we can run our data through validators before creating the table data
+        validate: {
+            isEmail: true
+        }
+    },
+    // define a password column
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [4]
+        }
+    }
   },
   {
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
