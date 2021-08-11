bd1c4edaccef27962a739f2dc8b9e21e5e6d8a39
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-08-11 11:32:49 -0400

    post routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// The constraint we impose here is that a post can belong to one user, but not many users
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 module.exports = { User, Post };
