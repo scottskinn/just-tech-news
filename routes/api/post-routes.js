dfa82e2d74e9959cd35d59acc0e7fe5e97bcc0de
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-08-16 14:28:50 -0400

    vote refactor

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -11,7 +11,13 @@ router.get('/', (req, res) => {
     include: [
       {
         model: User,
-        attributes: ['username']
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
       }
     ]
   })
@@ -27,7 +33,13 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     include: [
       {
         model: User,
@@ -68,14 +80,16 @@ router.post('/', (req, res) => {
 // / PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
     // create the vote
-      // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-      .then(updatedPostData => res.json(updatedPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+
+    // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+  });
 });
+
 
 router.put('/:id', (req, res) => {
   Post.update(
