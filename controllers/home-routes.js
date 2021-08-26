a18b85536e3892e40a0845520428c2c94d248a19
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-08-26 09:57:43 -0400

    render login links

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -49,16 +49,20 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/', (req, res) => {
+    // res.render('homepage', {
+    //     id: 1,
+    //     post_url: 'https://handlebarsjs.com/guide/',
+    //     title: 'Handlebars Docs',
+    //     created_at: new Date(),
+    //     vote_count: 10,
+    //     comments: [{}, {}],
+    //     user: {
+    //         username: 'test_user'
+    //     }
+    // })
     res.render('homepage', {
-        id: 1,
-        post_url: 'https://handlebarsjs.com/guide/',
-        title: 'Handlebars Docs',
-        created_at: new Date(),
-        vote_count: 10,
-        comments: [{}, {}],
-        user: {
-            username: 'test_user'
-        }
+      posts,
+      loggedIn: req.session.loggedIn
     });
 });
 
