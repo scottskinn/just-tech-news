87bf3bf82fec9d461c76fdcc596b00e72e93a40d
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-09-12 20:36:20 -0400

    edit and delete

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,12 +1,9 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
+const { Post, User, Comment, Vote } = require("../models");
 const withAuth = require('../utils/auth');
 
-// router.get('/', withAuth, (req, res) => {
-//     res.render('dashboard', { loggedIn: true });
-// });
-
+// get all posts for dashboard
 router.get('/', withAuth, (req, res) => {
   console.log(req.session);
   console.log('======================');
